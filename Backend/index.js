a6f150e44bb80dc035b52e54b834d2d38d998867
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2024-12-07 19:21:15 +0530

    Ayo

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,11 @@
-import express from 'express';
-import { Server } from 'socket.io';
-import http from 'http';
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",  // or specify allowed origins
-  }
-});
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.on('message', (msg) => {
-    io.emit('message', msg);  // Broadcast message to all clients
-  });
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
-
-server.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+import { Server } from 'socket.io'; 
+const io = new Server(3000); 
+io.on('connection', (socket) => { 
+console.log('A user connected'); 
+socket.on('message', (msg) => { 
+io.emit('message', msg); 
+}); 
+socket.on('disconnect', () => { 
+console.log('A user disconnected'); 
+}); 
+}); 
